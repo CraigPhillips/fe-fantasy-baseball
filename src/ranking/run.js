b95e0d76b633f4584000f44c0a47319f7ace32a2
Craig Phillips <craigap@uw.edu>
2018-03-19 09:57:38 -0700

    updated with new projections

--- src/ranking/run.js
+++ src/ranking/run.js
@@ -3,7 +3,7 @@ const argv = require('yargs').argv;
 const SteamerProjections = require('../projections/steamer-projections');
 const ValueAboveAverage = require('./value-above-replacement');
 
-const steamerProjections = new SteamerProjections('projection-data/2018/02-25');
+const steamerProjections = new SteamerProjections('projection-data/2018/03-19');
 const valueAboveAverage = new ValueAboveAverage(11, 8, 10);
 
 const combinedRanks = valueAboveAverage.applyTo(
